f6f0046df2fc69bd4467e8423ea0fb17816a5df0
Stefan Samson <ss42701@outlook.com>
2026-03-23 10:48:03 +0100

    feat: check MX records before sending emails

---
+++ src/lib/email/receives.js
@@ -0,0 +1,15 @@
+import { resolveMx } from "node:dns/promises"
+
+/**
+ * @async
+ * @function receivesEmail
+ * @param {string} hostname 
+ * @returns {Promise<boolean>}
+ */
+export default async function receivesEmail(hostname) {
+  try {
+    return (await resolveMx(hostname)).length > 0
+  } catch {
+    return false
+  }
+}
--- src/lib/otp/send.js
+++ src/lib/otp/send.js
@@ -1,12 +1,20 @@
+import receivesEmail from "#src/lib/email/receives"
+
 /**
- * Implement here the sending of the OTP code to the credential/ID.
- * 
  * @async
- * @param {(string|number)} credential - Client credential/ID.
+ * @param {string} email - Email address.
  * @param {string} otp - OTP string code.
  * @returns {Promise<boolean>} Indicates whether the sending was successful or not. In case an internal error happened, just throw it.
  */
-export default async function sendOtp(credential, otp) {
+export default async function sendOtp(email, otp) {
+
+  const hostname = email.split("@")?.[1]
+
+  if (!hostname || !await receivesEmail(hostname)) {
+    return false
+  }
+
+  // Set logic to send the OTP code to the email address.
 
   return true
 
