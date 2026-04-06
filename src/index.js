63cacbfabcf4d2a7993424e80cb73779668046bd
Stefan Samson <ss42701@outlook.com>
2026-04-06 20:30:01 +0200

    feat: listen to SIGINT and SIGTERM

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ import OPTIONS from "#src/options"
 const server = Bun.serve(OPTIONS)
 
 console.log(server.url.origin)
+
+async function shutdown() {
+  console.log("Stopping server gracefully...")
+  await server.stop() 
+  console.log("Server successfully stopped.")
+  process.exit(0)
+}
+
+process.on("SIGINT", shutdown)
+process.on("SIGTERM", shutdown)
