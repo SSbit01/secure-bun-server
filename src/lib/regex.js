e0335504e746bbfe0ad695602d031a479c770277
Stefan Samson <ss42701@outlook.com>
2026-04-18 00:41:43 +0200

    refactor

--- src/lib/regex.js
+++ src/lib/regex.js
@@ -1,7 +1,7 @@
-import nameAttributes from "#shared/display.json";
+import displayNameAttributes from "#shared/display.json";
 import otpAttributes from "#shared/otp.json";
 
 export const regexBase64Url = /^[\w-]+$/;
-export const regexDisallowedDisplayNameCharacters = new RegExp(nameAttributes.regexDisallowed, "gu");
+export const regexDisallowedDisplayNameCharacters = new RegExp(displayNameAttributes.regexDisallowed, "gu");
 export const regexOtp = new RegExp(otpAttributes.regex);
 export const regexSeparators = /\p{Z}+/gu;
