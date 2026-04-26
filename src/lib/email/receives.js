a5ba2e082f58f7268cc64ec47ef7e317ec9bd09e
Stefan Samson <ss42701@outlook.com>
2026-04-26 15:46:46 +0200

    feat: set DNS servers

--- src/lib/email/receives.js
+++ src/lib/email/receives.js
@@ -1,4 +1,18 @@
-import { resolveMx } from "node:dns/promises";
+import { resolveMx, setServers } from "node:dns/promises";
+
+setServers([
+  // Quad9
+  "9.9.9.9",
+  "149.112.112.112",
+
+  // Cloudflare (protection against malware and adult content)
+  "1.1.1.3",
+  "1.0.0.3",
+
+  // Cloudflare (protection against malware)
+  "1.1.1.2",
+  "1.0.0.2"
+]);
 
 /**
  * @async
