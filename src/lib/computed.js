84361dfbfda38cfe45e38c31e050bee2761d3705
Stefan Samson <ss42701@outlook.com>
2026-05-17 01:24:10 +0200

    style

--- src/lib/computed.js
+++ src/lib/computed.js
@@ -5,15 +5,9 @@ import { OTP_INVALID_BLOCK_SECONDS, OTP_MAX_AGE, OTP_RESEND_BLOCK_SECONDS } from
 import { SESSION_MAX_AGE } from "#src/lib/session/custom";
 import { secondsToMs } from "#src/lib/time";
 
-/**
- * @type {number}
- */
 export const KEK_ID_LENGTH = bytesToBase64Length(KEK_ID_BYTES);
-
 export const ENVELOPE_ENCRYPTION_WRAP_LENGTH = KEK_ID_LENGTH + bytesToBase64Length(WRAPPED_DEK_BYTES);
-
 export const OTP_INVALID_BLOCK_MS = secondsToMs(OTP_INVALID_BLOCK_SECONDS);
 export const OTP_MAX_AGE_MS = secondsToMs(OTP_MAX_AGE);
 export const OTP_RESEND_BLOCK_MS = secondsToMs(OTP_RESEND_BLOCK_SECONDS);
-
 export const SESSION_MAX_AGE_MS = secondsToMs(SESSION_MAX_AGE);
